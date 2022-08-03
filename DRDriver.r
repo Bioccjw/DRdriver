7e460acef497e6f61140913eb22b4a394a6edef8
Wei Jiang <103645262+Bioccjw@users.noreply.github.com>
2022-08-03 11:37:47 +0800

    Delete DRDriver.r

--- DRDriver.r
+++
@@ -1,8 +0,0 @@
-DRdriver <- function(sample_ID){
-    network <- specific_network(sample_ID)
-	score <- data.frame(unique(network[,1]))
-	if(nrow(score)>1&length(unique(network[,3]))>1){
-	    driver <- Genetic_Algorithm(network,score)
-		return(driver)
-	}
-}
