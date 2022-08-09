e65926a57e1dc385e0a7c6ac8e672f8624fb6a1d
Wei Jiang <103645262+Bioccjw@users.noreply.github.com>
2022-08-09 21:47:37 +0800

    Update specific_network.r

--- specific_network.r
+++ specific_network.r
@@ -1,6 +1,6 @@
 specific_network <- function(sample_ID){
-    diff_mutation <- differential_mutation(sample_ID)
-	DEG <- differential_exp(sample_ID)
+        diff_mutation <- differential_mutation(sample_ID)
+        DEG <- differential_exp(sample_ID)
 	sample_network_1 <- GRN[which(!is.na(match(GRN[,1],diff_mutation[,2]))),]
 	sample_network <- sample_network_1[which(!is.na(match(sample_network_1[,2],rownames(resistant_exp)))),]
 	sample_network[,3] <- rep("non-diff",nrow(sample_network))
